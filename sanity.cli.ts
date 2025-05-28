825b3efb9d81aa5c99b0399cd917d7c094a8fe2e
Ernie1234 <anumahjoshuaeneye@gmail.com>
2025-05-28 15:04:35 +0100

    fix: fix the sanity studio deployment error

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -4,8 +4,12 @@
  **/
 import { defineCliConfig } from "sanity/cli";
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+const projectId =
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ||
+  process.env.SANITY_STUDIO_SANITY_PROJECT_ID;
+const dataset =
+  process.env.NEXT_PUBLIC_SANITY_DATASET ||
+  process.env.SANITY_STUDIO_SANITY_DATASET;
 const studioHost = process.env.NEXT_PUBLIC_SANITY_STUDIO_HOSTNAME;
 
 export default defineCliConfig({
